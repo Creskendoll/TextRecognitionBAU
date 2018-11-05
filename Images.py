a6fc2128ef59a47d0e690bbc8ab5bab597ca4a41
Kenan Soylu <ken.soylu@hotmail.com>
2018-11-05 22:51:14 +0300

    loads of changes including the load I put in yOuR MoM

---
+++ Images.py
@@ -0,0 +1,79 @@
+import os
+import cv2
+
+# an in memory object whioch lets us access images much faster than we'd do with reading them out of disk
+# costs a little time at statup
+# {count} optional variable can be set to read defined number of images, -1 is for all
+class Images(object):
+    # Load the images in folder
+    def __init__(self, folder_name, grayscale=True, sub_folders=False, count=-1):
+        print("Loading folder:", os.path.abspath(folder_name))
+        # create a dictionary of images with their names and actual data
+        self.images = {}
+        # define directories depending on how many folders we wanna dig in to
+        if sub_folders:
+            directories = self.os.walk(folder_name)
+        else:
+            directories = self.walklevel(folder_name)
+
+        for root, dirs, files in directories:
+            for file_name in files:
+                if count != 0:
+                    count = count - 1
+                    if grayscale:
+                        img = cv2.imread(os.path.join(root, file_name), cv2.IMREAD_GRAYSCALE)
+                    else:
+                        img = cv2.imread(os.path.join(root, file_name), cv2.IMREAD_COLOR)
+                    
+                    if img is None:
+                        print("Could not read image:", file_name)
+                    else:
+                        # save name and data to the data itself so that it's faster to read from memory
+                        self.images[file_name] = img
+    
+    def save_all_images(self, path):
+        for image_name in self.images.keys():
+            self.save_image_by_name(path, img_name)
+
+    def save_image_by_name(self, path, image_name):
+        if not os.path.isdir(path):
+            print("Creating directory:", os.path.abspath(path))
+            os.mkdir(path)
+        out_file_name = image_name.split(".")[0] + "_out.png"
+        print("Saving image as:", out_file_name) 
+        cv2.imwrite(path + out_file_name, self.images[image_name])
+
+    # path includes the file name
+    def save_image(self, path, image):
+        directory = os.path.dirname(path)
+        if not os.path.isdir(directory):
+            print("Creating directory:", os.path.abspath(directory))
+            os.mkdir(directory)
+        # Edit this line if you wanna change output name
+        out_file_name = path[len(directory)+1:].split(".")[0] + "_out.png"
+        print("Saving image as:", out_file_name) 
+        cv2.imwrite(os.path.join(directory, out_file_name), image)
+
+    # window name is the name given in cv2.namedWindow({name}, flag)
+    def show_image(self, window_name, image_name):
+        img = self.images[image_name]
+        if img is None:
+            print("No image named %s in collection" % image_name)
+        else:
+            # create window if doesn't exist
+            if cv2.getWindowProperty(window_name, 0) < 0:
+                cv2.namedWindow(window_name, cv2.WINDOW_AUTOSIZE)
+            cv2.imshow(window_name, img)
+
+    # same with os.walk
+    # level is how deep we wanna go in the recursion
+    def walklevel(self, some_dir, level=1):
+        some_dir = some_dir.rstrip(os.path.sep)
+        assert os.path.isdir(some_dir)
+        num_sep = some_dir.count(os.path.sep)
+        for root, dirs, files in os.walk(some_dir):
+            yield root, dirs, files
+            num_sep_this = root.count(os.path.sep)
+            if num_sep + level <= num_sep_this:
+                del dirs[:]
+                
--- preProcess.py
+++ preProcess.py
@@ -2,6 +2,7 @@ import os
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+from Images import Images
 
 in_folder = "./in/"
 out_folder = "./out/"
@@ -10,6 +11,13 @@ binary_threshold = 120
 dilatation_size = 1
 canny_min = 200
 canny_max = 250
+
+# Set {count} optional variable to read that many images 
+# Read all images available
+images_obj = Images(in_folder)
+
+global_img = None
+refPt = []
 
 def changeBinaryThreshold(val):
     global binary_threshold
@@ -27,58 +35,96 @@ def changeDilationSize(val):
     global dilatation_size
     dilatation_size = val
 
+# Process clicks on the image 
+# must be set to a callback in order to work
+# cv2.setMouseCallback("Images", click) (see below)
+def click(event, x, y, flags, param):
+    global global_img, refPt
+    if event == cv2.EVENT_LBUTTONDOWN:
+        # record first (x, y)
+        refPt = [(x, y)]
+    elif event == cv2.EVENT_LBUTTONUP:
+        # record the ending (x, y)
+        refPt.append((x, y))
+        print("Click Up", refPt)
+        # draw a rectangle around the region of interest
+        # works a bit weird with grayscale 
+        cv2.rectangle(global_img, refPt[0], refPt[1], (255, 255, 255), 6)
 
 cv2.namedWindow("Images", cv2.WINDOW_AUTOSIZE)
 
 cv2.createTrackbar('Binary Threshold', 'Images', 120, max_binary_threshold, changeBinaryThreshold)
 cv2.createTrackbar('Canny Min', 'Images', 200, 900, changeCannyMin)
 cv2.createTrackbar('Canny Max', 'Images', 250, 900, changeCannyMax)
-cv2.createTrackbar('Dilation Size', 'Images', dilatation_size, 20, changeDilationSize)
+cv2.createTrackbar('Dilation Size', 'Images', 2, 5, changeDilationSize)
 
-for root, dirs, files in os.walk(in_folder):
-    for file_name in files:
-        print("Processing:", file_name)
-            
-        # Read and convert the image to grayscale
-        img_gray = cv2.imread(os.path.join(root, file_name), cv2.IMREAD_GRAYSCALE)
-        if img_gray is None:
-            print('Could not open or find the image: ', file_name)
-            exit(0)
-        # Convert image to binary
-        # Automaticly determines threshold 
-        # (binary_threshold, img_binary) = cv2.threshold(img_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-        # uses the defined threshold 
-        img_binary = cv2.threshold(img_gray, binary_threshold, 255, cv2.THRESH_BINARY)[1]
+# Uncomment this if u want to utilize mouse clicks  
+# cv2.setMouseCallback("Images", click)
 
-        # Values from the example online
-        # TODO: Explain wtf the canny values do
-        # https://docs.opencv.org/3.1.0/da/d22/tutorial_py_canny.html
-        # Edge detection
-        laplacian_edge = cv2.Laplacian(img_binary,cv2.CV_64F)
-        sobelx_edge = cv2.Sobel(img_binary,cv2.CV_64F,1,0,ksize=5)  # x
-        sobely_edge = cv2.Sobel(img_binary,cv2.CV_64F,0,1,ksize=5)  # y
+# define all pre processing stuff under this function
+def apply_pre_processing(image, resize_by=1, binary=False):
+    # so that we don't alter the original image
+    new_image = image.copy()
 
-        # Show cv2 windows with trackbars n shit
-        while True:
-            canny_edge = cv2.Canny(img_gray, canny_min, canny_max)
-            # Dilation 
-            # https://docs.opencv.org/3.4/db/df6/tutorial_erosion_dilatation.html
-            element = cv2.getStructuringElement(cv2.MORPH_RECT, (2*dilatation_size+1, 2*dilatation_size+1), (dilatation_size, dilatation_size))
-            dilatation_dst = cv2.dilate(canny_edge, element)
-            cv2.imshow('Images', dilatation_dst)
-            # Exit when Esc is pressed
-            k = cv2.waitKey(1) & 0xFF
-            if k == 27: 
-                break
+    new_image = new_image[240:320, 670:1250] # RoI
 
-        # Show images
-        # plt.subplot(131),plt.imshow(img_gray, cmap=plt.cm.binary)
-        # plt.title('Gray Image'), plt.xticks([]), plt.yticks([])
-        # plt.subplot(132),plt.imshow(canny_edge, cmap='gray')
-        # plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
-        # plt.subplot(133),plt.imshow(dilatation_dst, cmap='gray')
-        # plt.title('Dilated Image'), plt.xticks([]), plt.yticks([])
-        # plt.show()
+    new_image = cv2.resize(new_image, (0,0), fx=resize_by, fy=resize_by) 
+
+    # Convert image to binary
+    # Automaticly determines threshold 
+    # uses the defined threshold 
+    if binary:
+        # (binary_threshold, img_binary) = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+        new_image = cv2.threshold(new_image, binary_threshold, 255, cv2.THRESH_BINARY)[1]
+
+    # TODO: Explain wtf the canny values do
+    # https://docs.opencv.org/3.1.0/da/d22/tutorial_py_canny.html
+    # Note: We got rid of laplacian and sobel since we won't necesseraly talk about them in our report
+    # Sobel we will but it will be covered under Canny 
+    # Edge detection
+    canny_edge_img = cv2.Canny(new_image, canny_min, canny_max)
+
+    # Dilation 
+    # https://docs.opencv.org/3.4/db/df6/tutorial_erosion_dilatation.html
+    element = cv2.getStructuringElement(cv2.MORPH_DILATE, (2*dilatation_size+1, 2*dilatation_size+1), (dilatation_size, dilatation_size))
+    new_image = cv2.dilate(canny_edge_img, element)
+
+    # Show images if needed
+    # plt.subplot(131),plt.imshow(img_gray, cmap=plt.cm.binary)
+    # plt.title('Gray Image'), plt.xticks([]), plt.yticks([])
+    # plt.subplot(132),plt.imshow(canny_edge, cmap='gray')
+    # plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+    # plt.subplot(133),plt.imshow(dilatation_dst, cmap='gray')
+    # plt.title('Dilated Image'), plt.xticks([]), plt.yticks([])
+    # plt.show()
+
+    return new_image
+
+def apply_to_all_and_save():
+    for img_name, img in images_obj.images.items():
+        images_obj.save_image(out_folder + img_name, apply_pre_processing(img))
 
 
+for img_name, img in images_obj.images.items():
+    global global_img
+    print("Showing image:", img_name)
 
+    # Show cv2 windows with trackbars n shit
+    while True:
+        processed_image = apply_pre_processing(img, resize_by=3)
+        
+        # for mouse clicks
+        # global_img = processed_image
+        
+        cv2.imshow('Images', processed_image)
+
+        # Exit when Esc is pressed
+        k = cv2.waitKey(1) & 0xFF
+        # Save individual image when s is pressed 
+        if k == ord("s"):
+            images_obj.save_image(out_folder + img_name, processed_image)
+        # (p)rocess and save all the images in the in folder to the our folder  
+        if k == ord("p"):
+            apply_to_all_and_save()
+        if k == ord("q") or k == 27:
+            break
