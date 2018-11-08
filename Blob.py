25d25bac22efb96088a8e642ab30d6f5735969ca
Kenan Soylu <ken.soylu@hotmail.com>
2018-11-08 23:41:51 +0300

    start blob detection

--- Blob.py
+++ Blob.py
@@ -1,27 +1,21 @@
-from skimage.filters import threshold_otsu
 from skimage.segmentation import clear_border
 from skimage.measure import label, regionprops
-from skimage.morphology import closing, square
-from skimage.color import label2rgb
 
 import matplotlib.patches as mpatches
 
 class Blob(object):
     # input image must be BW
-    def __init__(self, image_bw, thresh=100):
+    def __init__(self, image_bw):
         # remove artifacts connected to image border
         cleared = clear_border(image_bw)
-
         # label image regions
         self.label_image = label(cleared)
-        image_label_overlay = label2rgb(self.label_image, image=image_bw)
-        self.labeled_image = image_label_overlay
 
-    def getBoundingBox(self, region_area=100):
+    def getBoundingBox(self, region_area=80):
         for region in regionprops(self.label_image):
             # take regions with large enough areas
             if region.area >= region_area:
                 # draw rectangle around segmented coins
                 minr, minc, maxr, maxc = region.bbox
                 yield mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
-                                        fill=False, edgecolor='red', linewidth=2)
+                                        fill=False, edgecolor='red', linewidth=1)
--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from Images import Images
 from Blob import Blob
 import matplotlib.pyplot as plt
+import cv2
 
 out_folder = "./out" 
 in_folder = "./in"
+fig,ax = plt.subplots(1)
+images_obj = Images(out_folder, count=1)
 
-if __name__ == "main":
-    images_obj = Images(out_folder)
-    for img_name, img in images_obj.images.items():
-        blob = Blob(img)
-        b_boxes = blob.getBoundingBox()
+for img_name, img in images_obj.images.items():
+    img = cv2.resize(img, (0,0), fx=2, fy=2)
+    blob = Blob(img)
+    b_boxes = blob.getBoundingBox()
+    ax.imshow(img, cmap=plt.cm.binary)
+    for rect in b_boxes:
+        ax.add_patch(rect)
+    plt.show()
+
