41739bc8efea6a1b3b795c6e8bfa216366838606
Kenan Soylu <ken.soylu@hotmail.com>
2018-11-14 12:06:21 +0300

    undemo

--- preProcess.py
+++ preProcess.py
@@ -16,7 +16,7 @@ canny_max = 500
 
 # Set {count} optional variable to read that many images
 # Read all images available
-images_obj = Images(in_folder, grayscale=True, count=5)
+images_obj = Images(in_folder, grayscale=True)
 
 global_img = None
 refPt = []
@@ -86,62 +86,61 @@ def flood_fill(img,h_max=255):
 # define all pre processing stuff under this function
 def apply_pre_processing(image, resize_by=1):
     # so that we don't alter the original image
-    new_image = image.copy()
+    new_img = image.copy()
+    new_img = new_img[250:300, 685:1230] # RoI
 
-    new_image = cv2.resize(new_image, (0,0), fx=resize_by, fy=resize_by)
+    new_img = cv2.resize(new_img, (0,0), fx=resize_by, fy=resize_by)
     
     # Easy way
-    # ret,th = cv2.threshold(new_image,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-    # return th
+    ret,th = cv2.threshold(new_img,binary_threshold,255,cv2.THRESH_BINARY)
+    return th
 
     # TODO: Explain wtf the canny values do
     # https://docs.opencv.org/3.1.0/da/d22/tutorial_py_canny.html
     # Note: We got rid of laplacian and sobel since we won't necesseraly talk about them in our report
     # Sobel we will but it will be covered under Canny
     # Edge detection
-    canny_edge_img = cv2.Canny(new_image, canny_min, canny_max)
+    canny_edge_img = cv2.Canny(new_img, canny_min, canny_max)
     
     # Dilation
     # https://docs.opencv.org/3.4/db/df6/tutorial_erosion_dilatation.html
     element = cv2.getStructuringElement(cv2.MORPH_DILATE, (dilatation_size+1, dilatation_size+1), (dilatation_size, dilatation_size))
     new_image = cv2.dilate(canny_edge_img, element)
 
-    return new_image
+    return new_img
 
 def apply_to_all_and_save():
     for img_name, im in images_obj.images.items():
-        p_im = flood_fill(apply_pre_processing(im)) 
+        p_im = apply_pre_processing(im) 
+        f_im = flood_fill(p_im) 
         images_obj.save_image(out_folder + img_name, p_im)
 
 for img_name, img in images_obj.images.items():
     print("Showing image:", img_name)
     # new_image = new_image[240:320, 670:1250] # RoI
-    img = img[250:300, 680:1230] # RoI
-
-    resize_img_by = 2
-    
-    img = cv2.resize(img, (0,0), fx=resize_img_by, fy=resize_img_by)
+    # img = img[250:300, 680:1230] # RoI
 
     # Show cv2 windows with trackbars n shit
     while True:
         # for mouse clicks
         # global_img = processed_image
-        processed_image = apply_pre_processing(img)
+        processed_image = apply_pre_processing(img, resize_by=2)
 
         if fill:
             processed_image = flood_fill(processed_image)
 
         # OTSU is automatic
         # This is manual thresholding 
-        _,bin_image = cv2.threshold(img,binary_threshold,255,cv2.THRESH_BINARY)
+        # _,bin_image = cv2.threshold(img,binary_threshold,255,cv2.THRESH_BINARY)
     
         # original, grayscale, binary, canny
-        stacked_image = np.vstack((img, bin_image, processed_image))
+        # stacked_image = np.vstack((img, bin_image, processed_image))
         
-        cv2.imshow('Images', stacked_image)
+        cv2.imshow('Images', processed_image)
 
         # Exit when Esc is pressed
         k = cv2.waitKey(1) & 0xFF
+        # print(k)
         # Save individual image when s is pressed
         if k == ord("s"):
             images_obj.save_image(out_folder + img_name, processed_image)
